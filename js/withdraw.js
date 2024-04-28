e0ab209bdc37a16c63c7eabc03f4694999aee1eb
Maasuuum <fardousalmasum09@gmail.com>
2024-04-28 15:36:20 +0600

    some irror

--- js/withdraw.js
+++ js/withdraw.js
@@ -19,24 +19,35 @@ document.getElementById('btn-withdraw').addEventListener('click', function(){
     const newWithDrawAmountString = withdrawField.value; 
     const newWithDrawAmount = parseFloat(newWithDrawAmountString);
 
+    // step-7: 
+    withdrawField.value = '';
+
+    if(isNaN(newWithDrawAmount)){
+        alert('Please provide a valid number');
+        return;
+    }
+
     // step-3:
     const withdrawTotalElement = document.getElementById('withdraw-total');
     const previousWithdrawTotalString = withdrawTotalElement.innerText;
     const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
-
-    // step-4:
-    const currentWithdrawTotal = previousWithdrawTotal + newWithDrawAmount;
-    withdrawTotalElement.innerText = currentWithdrawTotal;
 
     // step-5:
     const balanceTotalElement = document.getElementById('balance-total');
     const previousBalanceTotalString = balanceTotalElement.innerText;
     const previousBalanceTotal = parseFloat(previousBalanceTotalString);
 
+
+    if(newWithDrawAmount > previousBalanceTotal){
+        alert('Baap er bank e eto taka nai');
+        return;
+    }
+
+    // step-4:
+    const currentWithdrawTotal = previousWithdrawTotal + newWithDrawAmount;
+    withdrawTotalElement.innerText = currentWithdrawTotal;
+
     // step-6:
     const newBalanceTotal = previousBalanceTotal - newWithDrawAmount;
     balanceTotalElement.innerText = newBalanceTotal;
-
-    // step-7: 
-    withdrawField.value = '';
 })
